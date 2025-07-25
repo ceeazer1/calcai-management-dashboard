365f603185a7b427d8a9a8eade44cf6dead30052
ceeazer1 <ceeazer1@gmail.com>
2025-07-25 15:53:15 -0400

    Update ESP32 secrets configuration

--- esp32/secrets.h
+++ esp32/secrets.h
@@ -6,7 +6,7 @@
 
 #define WIFI_SSID "" // Your wifi name
 #define WIFI_PASS "" // Your wifi password
-#define SERVER "" // Your server ip address
+#define SERVER "https://calcai-server.vercel.app/" // Your server ip address
 #define CHAT_NAME "" // Your name on the chat program
 // #define SECURE // not used
 
