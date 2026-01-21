05d8ffc7ab21e0c5e9578f7c52702aec28dc955b
CalcAI Auto Agent <auto@calcai.cc>
2026-01-20 20:20:51 -0500

    Remove temporary script inspect_kv.ts fixing build

--- inspect_kv.ts
+++
@@ -1,1 +0,0 @@
-import { getKvClient } from '@/lib/kv'; const kv=getKvClient(); const orders = await kv.get('orders:square:imported'); console.log(JSON.stringify(orders ? orders.slice(0, 3) : [], null, 2));
